56dde2ed7ed8c42f52dbd3fb44ad376f86b11381
waleedkhan8890 <waleedkhan76089@gmail.com>
2025-07-11 18:16:37 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "962791578018"
+global.ownername = "🐛 ANONYMOUS "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
-global.botname = 'WASI TECH BUG BOT ' //name of the bot
+global.ownernumber = '962791578018'  //creator number
+global.ownername = ' ANONYMOUS ' //owner name
+global.botname = 'ANONYMOUS BUG BOT ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
